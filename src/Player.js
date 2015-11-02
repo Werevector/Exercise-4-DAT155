4952cf70f1d673a03860445110864ca6063081e3
Werevector <runartistel@gmail.com>
2015-11-02 13:51:29 +0100

    added basis for player

---
+++ src/Player.js
@@ -0,0 +1,17 @@
+
+
+var Player = function(){
+  this._scene;
+  this._camera;
+  this._model = _loadPlayerModel('resources/models/player.js');
+}
+
+Player.prototype.move(){
+
+}
+
+Player.prototype.turn(){
+
+}
+
+Player.prototype._loadPlayerModel();
